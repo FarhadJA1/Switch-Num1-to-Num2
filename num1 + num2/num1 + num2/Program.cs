5ff35ed0306b26df8ef7aa884122a1e98e5e933f
FarhadJA1 <farhadja@code.edu.az>
2021-11-25 23:45:43 +0400

    Num1 and Num2 added

---
+++ num1 + num2/num1 + num2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace num1___num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 11;
+            switch (num1)
+            {
+                case 5:
+                    num1 = 11;
+                    break;
+
+                
+            }
+            switch (num2)
+            {
+                case 11:
+                    num2 = 5;
+                    break;
+
+                default:
+                    break;
+                    
+            }
+            Console.WriteLine(num1 + " " + num2);
+        }
+    }
+}
